2cfb5774720e65b78deed6183e3867488639c0a4
Jeon Hyewon <107667966+hyennii@users.noreply.github.com>
2024-03-13 15:04:15 +0900

    life_in_weeks 출력

---
+++ section01~2/life_in_weeks.py
@@ -0,0 +1,10 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+
+# 90살까지만 살 수 있다고 가정하였을때, 남은 수명을 주수로 나타내기
+
+years = 90 - int(age)
+weeks = years * 52
+
+print(f"You have {weeks} weeks left.")
