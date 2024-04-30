7abe90d3cec3b24f7219b41578ca84b94fb7d80b
Jeon Hyewon <107667966+hyennii@users.noreply.github.com>
2024-04-30 16:02:44 +0900

    가장 큰 숫자

---
+++ section05~6/high_score.py
@@ -0,0 +1,12 @@
+## 리스트에서 가장 큰 숫자 구하기
+
+student_scores = input().split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+
+highest_score = 0
+for score in student_scores :
+  if score > highest_score : 
+    highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
